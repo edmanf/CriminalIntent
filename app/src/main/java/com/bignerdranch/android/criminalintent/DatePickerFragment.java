9e08ae82bc52c2590574869e1a611c0a831a533b
Edman Feng <edmanfeng@hotmail.com>
2017-01-12 12:34:09 -0800

    12.9 Done

--- app/src/main/java/com/bignerdranch/android/criminalintent/DatePickerFragment.java
+++ app/src/main/java/com/bignerdranch/android/criminalintent/DatePickerFragment.java
@@ -1,17 +1,22 @@
 package com.bignerdranch.android.criminalintent;
 
 import android.app.Dialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AlertDialog;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.DatePicker;
+import android.widget.EditText;
 
 import java.util.Calendar;
 import java.util.Date;
 
 public class DatePickerFragment extends DialogFragment {
+
+    public static final String EXTRA_DATE =
+            "com.bignerdranch.android.crimnalintent.date";
 
     private static final String ARG_DATE = "date";
 
@@ -49,5 +54,19 @@ public class DatePickerFragment extends DialogFragment {
                 .setTitle(R.string.date_picker_title)
                 .setPositiveButton(android.R.string.ok, null)
                 .create();
+    }
+
+    private void sendResult(int resultCode, Date date) {
+        if (getTargetFragment() == null) {
+            return;
+        }
+
+        Intent intent = new Intent();
+        intent.putExtra(EXTRA_DATE, date);
+
+        // Target fragment was set by CrimeFragment to be CrimeFragment,
+        // so use its onActivityResult to pass back an intent
+        getTargetFragment()
+                .onActivityResult(getTargetRequestCode(), resultCode, intent);
     }
 }
