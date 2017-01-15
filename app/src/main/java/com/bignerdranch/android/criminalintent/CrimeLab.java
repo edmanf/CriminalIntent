df4c898dceefff3ca219c408505a1bb84a6b2766
Edman Feng <edmanfeng@hotmail.com>
2017-01-15 15:26:32 -0800

    13.10-13.11 Done

--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimeLab.java
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeLab.java
@@ -16,12 +16,6 @@ public class CrimeLab {
 
     private CrimeLab(Context context) {
         mCrimes = new ArrayList<Crime>(); //new ArrayList<>() is okay in java 7+
-        for (int i = 0; i < 100; i++) {
-            Crime crime = new Crime();
-            crime.setTitle("Crime #" + i);
-            crime.setSolved(i % 2 == 0);
-            mCrimes.add(crime);
-        }
     }
 
     public static CrimeLab get(Context context) {
@@ -31,6 +25,9 @@ public class CrimeLab {
         return sCrimeLab;
     }
 
+    public void addCrime(Crime c) {
+        mCrimes.add(c);
+    }
 
     public List<Crime> getCrimes() {
         return mCrimes;
